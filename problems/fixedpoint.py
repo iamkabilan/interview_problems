af0da4fda811bb88c996956f44cefc4e1e3769ad
kabilan <38951107+iamkabilan@users.noreply.github.com>
2019-12-06 15:08:01 +0530

    Add files via upload

---
+++ problems/fixedpoint.py
@@ -0,0 +1,17 @@
+"""  This problem was recently asked by Apple:
+
+A fixed point in a list is where the value is equal to its index.
+So for example the list [-5, 1, 3, 4], 1 is a fixed point in the list
+since the index and value is the same. 
+Find a fixed point (there can be many,just return 1) in a sorted list of distinct elements,
+ or return None if it doesn't exist. """
+
+def find_fixed_point(nums):
+	for num,i in enumerate(nums):
+  		if(num==i):
+  			return 1
+  		
+
+
+
+print find_fixed_point([-5, 0, 3, 4])
